89c114cd32a13a4338d008f1329a6ed8f91c256d
Divesh Keswani <divkeswa@publicisgroupe.net>
2023-07-28 19:46:20 +0530

    COrs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 import express from "express"
+import cors from "cors"
 import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
 dotenv.config()
 const PORT = process.env.PORT || 3030;
 import {run2} from "./destination.js"
@@ -9,8 +11,13 @@ import { generateEmbeddings } from "./generateEmbeddings.js";
 
 const app = express();
 
+app.use(cors());
+
+
 // For parsing application/json
-app.use(express.json());
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 app.post("/langchain",async(req,res)=>{
 
@@ -19,9 +26,9 @@ app.post("/langchain",async(req,res)=>{
     const message = req.body?.message;
     const count = req.body?.count;
 
-    const result = await run2(req.body?.message,count);
+    const result = await run2(message,count);
     
-      res.json(result)
+      res.status(200).json(result)
   }
 
   catch(e){
