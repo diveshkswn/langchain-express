ce6cfdbe7af2fb2d24659e753deb2445eb82490a
Divesh Keswani <divkeswa@publicisgroupe.net>
2023-07-28 12:21:26 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,53 @@
+
+import express from "express"
+import * as dotenv from "dotenv";
+dotenv.config()
+const PORT = process.env.PORT || 3030;
+import {run2} from "./destination.js"
+import { generateEmbeddings } from "./generateEmbeddings.js";
+
+
+const app = express();
+
+// For parsing application/json
+app.use(express.json());
+
+app.post("/langchain",async(req,res)=>{
+
+  try{
+    console.log(req.body)
+
+    const result = await run2(req.body?.message);
+    
+      res.json(result)
+  }
+
+  catch(e){
+    res.status(500).json({message : `Error : ${e}`})
+  }
+
+})
+
+app.get("/generateEmbeddings",async(req,res)=>{
+
+  try{
+    console.log(req.body);
+
+    generateEmbeddings();
+    res.json({message : "Embeddings Generated"});
+  }
+
+  catch(e){
+    res.status(500).json({message : `Error : ${e}`})
+  }
+
+})
+
+
+
+// error handler
+
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
